389477f704f38811cb565e4d572904f1a46bfa45
andriimsn <andrewmosin8@gmail.com>
2021-08-07 10:00:05 +0300

    init

---
+++ 01-first-app/completed/build/precache-manifest.77267f194fa08fb0e534dc82622b883a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a253a05a5bf986db915a8de26707c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65346169ca3ccbfa5b9",
+    "url": "/static/css/main.65b8456e.chunk.css"
+  },
+  {
+    "revision": "3dba83de8a5748de6905",
+    "url": "/static/js/2.3b126b67.chunk.js"
+  },
+  {
+    "revision": "eadc8ac8b6080df510389a008a76ce0a",
+    "url": "/static/js/2.3b126b67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b65346169ca3ccbfa5b9",
+    "url": "/static/js/main.d87afc6d.chunk.js"
+  },
+  {
+    "revision": "d8f7e6ab5b514fbfded3",
+    "url": "/static/js/runtime-main.d06d07b5.js"
+  }
+]);
